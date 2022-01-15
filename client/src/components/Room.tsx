663609864a74fd91681534585a76e2d10abc76f6
unknown <pronojit@protonmail.ch>
2022-01-16 01:21:37 +0530

    strating room component

--- client/src/components/Room.tsx
+++ client/src/components/Room.tsx
@@ -1,15 +1,38 @@
 import React from "react";
 import HotelPage from "./Hotelpage";
 
+enum roomType {
+  "Delux",
+  "Non-delux",
+}
+
 interface RoomsProps {
-  room: any;
+  name: string;
+  description: string;
+  imageurls: [string];
+  maxcount: number;
+  phonenumber: number;
+  type: roomType;
+  rentperday: number;
 }
 // { rooms }:
 
-const Hotels = ({ room }: RoomsProps) => {
+const Hotels = ({
+  name,
+  description,
+  imageurls,
+  maxcount,
+  phonenumber,
+  type,
+  rentperday,
+}: RoomsProps) => {
   return (
     <div>
-      <h2>hotel</h2>
+      <h2>{name}</h2>
+      <img src={imageurls[0]} alt="room-image" />
+      <p>room capacity: {maxcount}</p>
+      <p>{type}</p>
+      <h3>{rentperday}</h3>
     </div>
   );
 };
