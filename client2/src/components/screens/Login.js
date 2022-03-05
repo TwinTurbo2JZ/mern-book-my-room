ea1a22d51dd2848266f49614d5827d83f65d9bec
DESKTOP-S04RBAC\Pron <pronojit@protonmail.ch>
2022-03-05 20:21:43 +0530

    added login register forms

--- client2/src/components/screens/Login.js
+++ client2/src/components/screens/Login.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState(" ");
+  const [password, setPassword] = useState(" ");
+
+  const LoginUser = () => {
+    const user = {
+      email,
+      password,
+    };
+
+    console.log(user);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input
+        type="text"
+        className=""
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="text"
+        className=""
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={LoginUser}>Login</button>
+    </div>
+  );
 };
 
 export default Login;
--- client2/src/components/screens/Register.js
+++ client2/src/components/screens/Register.js
@@ -1,13 +1,66 @@
 import React, { useState, useEffect } from "react";
 
 const Register = () => {
+  const [name, setName] = useState(" ");
+  const [email, setEmail] = useState(" ");
+  const [cemail, setCemail] = useState(" ");
+  const [password, setPassword] = useState(" ");
+  const [cpassword, setCpassword] = useState(" ");
+
+  const registerUser = () => {
+    if (password == cpassword) {
+      const user = {
+        name,
+        email,
+        cemail,
+        password,
+      };
+
+      console.log(user);
+    } else {
+      alert("Passwords don't match");
+    }
+  };
+
   return (
     <div>
       <h1>Register</h1>
-      <input type="text" className="" placeholder="name" />
-      <input type="text" className="" placeholder="email" />
-      <input type="text" className="" placeholder="confirm email" />
-      <input type="text" className="" placeholder="password" />
+      <input
+        type="text"
+        className=""
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        className=""
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="text"
+        className=""
+        placeholder="confirm email"
+        value={cemail}
+        onChange={(e) => setCemail(e.target.value)}
+      />
+      <input
+        type="text"
+        className=""
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="text"
+        className=""
+        placeholder="confirm password"
+        value={cpassword}
+        onChange={(e) => setCpassword(e.target.value)}
+      />
+      <button onClick={registerUser}>Register</button>
     </div>
   );
 };
