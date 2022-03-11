990bec9f7b7bc3de75af27419c56656aa3f5c730
DESKTOP-S04RBAC\Pron <pronojit@protonmail.ch>
2022-03-11 21:29:36 +0530

    basic login funtionality implemented

--- client2/src/components/screens/Login.js
+++ client2/src/components/screens/Login.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Login = () => {
   const [email, setEmail] = useState(" ");
   const [password, setPassword] = useState(" ");
 
-  const LoginUser = () => {
+  const LoginUser = async () => {
     const user = {
       email,
       password,
     };
 
     console.log(user);
+
+    try {
+      const result = await axios.post("http://localhost:5000/api/login", user)
+        .data;
+      console.log(result);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   return (
